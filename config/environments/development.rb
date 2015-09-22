9c1c7f659f18dc29516d3051569a9212b936f482
Luis Porras <lporras16@gmail.com>
2015-09-22 20:36:02 -0300

    removing byebug from config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.perform_deliveries = true
-  byebug
   config.action_mailer.smtp_settings = {
     :address              => 'smtp.gmail.com',
     :port                 => '587',
