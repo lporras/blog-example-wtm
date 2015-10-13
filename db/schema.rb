3d0d37c52dc1d0ecd429057a19cd1084fd259a83
Luis Porras <lporras16@gmail.com>
2015-10-13 20:32:56 -0300

    adding avatar field to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005223828) do
+ActiveRecord::Schema.define(version: 20151013233206) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -67,16 +67,12 @@ ActiveRecord::Schema.define(version: 20151005223828) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "avatar"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "provider"
-    t.string   "uid"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["provider"], name: "index_users_on_provider"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["uid"], name: "index_users_on_uid"
 
 end
